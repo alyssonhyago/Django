84f971c511646dea702de748e6fb9b8c512a059d
Alysson Hyago <alysson.oliveira@ee.ufcg.edu.br>
2022-10-18 19:10:04 -0300

    parte 2

--- Projeto Agenda/contatos/admin.py
+++ Projeto Agenda/contatos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Categoria, Contato
+# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'sobrenome','telefone','email','data_criacao')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome','sobrenome')
+    list_per_page = 10
+    search_fields = ('id', 'nome', 'sobrenome')
+admin.site.register(Categoria)
+admin.site.register(Contato,ContatoAdmin)
