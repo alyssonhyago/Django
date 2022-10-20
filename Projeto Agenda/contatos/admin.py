26864066e3d6c8c60a184bc5e2e25f9cd6673493
Alysson Hyago <alysson.oliveira@ee.ufcg.edu.br>
2022-10-20 15:26:14 -0300

    add parte final agenda

--- Projeto Agenda/contatos/admin.py
+++ Projeto Agenda/contatos/admin.py
@@ -6,10 +6,12 @@ from .models import Categoria, Contato
 # Register your models here.
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome', 'sobrenome','telefone','email','data_criacao')
+    list_display = ('id','nome', 'sobrenome','telefone','email','data_criacao', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
-    list_filter = ('nome','sobrenome')
+    #list_filter = ('nome','sobrenome')
     list_per_page = 10
     search_fields = ('id', 'nome', 'sobrenome')
+    list_editable = ('telefone', 'mostrar')
+    
 admin.site.register(Categoria)
 admin.site.register(Contato,ContatoAdmin)
